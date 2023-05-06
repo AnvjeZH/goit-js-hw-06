5ed022db384155b928ebac356124e885823a1bc6
Anzhelika Zhupynas <121193138+AnvjeZH@users.noreply.github.com>
2023-05-06 16:32:01 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,19 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+const bodyEl = document.body
+const btnEl = document.querySelector('.change-color')
+const textColorEl = document.querySelector('.color')
+
+btnEl.addEventListener('click', onChangeBackroundColor)
+
+function onChangeBackroundColor(event) {
+
+bodyEl.style.backgroundColor = getRandomHexColor()
+ const resultFn = getRandomHexColor()
+textColorEl.textContent = resultFn
 }
+
+function getRandomHexColor() {  
+return `#${Math.floor(Math.random() * 16777215)   
+.toString(16)    
+.padStart(6, 0)}`;
+}
+
