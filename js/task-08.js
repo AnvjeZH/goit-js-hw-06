671dd4f703160e5aa39019fb688b0552fa5c6f2e
Anzhelika Zhupynas <121193138+AnvjeZH@users.noreply.github.com>
2023-05-06 15:48:53 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.querySelector('.login-form')
+formEl.addEventListener('submit', onSubmitForm)
 
+function onSubmitForm (event) {
+event.preventDefault()
+
+const { elements : {email, password} } = event.currentTarget
+
+
+if(!email.value || !password.value) {
+return alert('Всі поля повинні бути заповнені!')
+}
+
+console.log(`{email: ${email.value}, password: ${password.value}}`)
+
+event.currentTarget.reset()
+}
