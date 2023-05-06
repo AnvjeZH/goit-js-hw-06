c246565ef7db254ebeb0ca5dcece1334881cf8fe
Anzhelika Zhupynas <121193138+AnvjeZH@users.noreply.github.com>
2023-05-06 12:05:51 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,15 @@
-rconst inputEl = document.getElementById('validation-input')
-inputEl.addEventListener('blur', onValidation)
+const inputEl = document.getElementById('validation-input')
+inputEl.addEventListener('blur', () => {
+      const inputLength = Number(inputEl.dataset.length)
+      const currentLength = inputEl.value.length
+            if (inputLength === currentLength) {
+            inputEl.classList.add('valid')
+            inputEl.classList.remove('invalid')
+            } else {
+            inputEl.classList.remove('valid')
+            inputEl.classList.add('invalid')
+            }
+    })
 
-function onValidation(event) {
-      const cur
-    if (event.target.value.lenght === 6) {
-          console.log('It' );  
-    }
-    return console.log('Not')
-    }
+      
 
